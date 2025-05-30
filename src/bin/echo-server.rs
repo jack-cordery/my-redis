dcfa980eba864e4c0a6451c2d6738d98ea79f462
Jack Cordery <jack.redolf@gmail.com>
2025-05-30 15:15:34 +0100

    example of how to use the write/write_all traits

--- src/bin/echo-server.rs
+++ src/bin/echo-server.rs
@@ -1,16 +1,17 @@
 use tokio::fs::File;
-use tokio::io;
+
+use tokio::io::{self, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let content = b"bello".repeat(100_000_000);
-    let mut reader: &[u8] = &content;
+    let content = b"bello\n".repeat(100_000_000);
+    let reader: &[u8] = &content;
 
     let mut file = File::create("foo.txt").await?;
 
-    let b = io::copy(&mut reader, &mut file).await?;
+    file.write_all(reader).await?;
 
-    println!("copy completed having copied {b} bytes");
+    println!("write completed");
 
     Ok(())
 }
