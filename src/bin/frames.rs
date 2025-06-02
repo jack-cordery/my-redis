d4e133757a272a7ad9113884fcb3a01caba19e2f
Jack Cordery <jack.redolf@gmail.com>
2025-06-02 14:40:36 +0100

    added read and write methods for redis protocol, tested with telnet

--- src/bin/frames.rs
+++ src/bin/frames.rs
@@ -1,23 +1,37 @@
-use bytes::BytesMut;
+use bytes::{Buf, BytesMut};
+use mini_redis::frame::Error::Incomplete;
 use mini_redis::{Frame, Result};
-use tokio::io::AsyncReadExt;
+use std::io::Cursor;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt, BufWriter};
 use tokio::net::{TcpListener, TcpStream};
 
 pub struct Connection {
-    stream: TcpStream,
+    stream: BufWriter<TcpStream>,
     buffer: BytesMut,
 }
 
 impl Connection {
     pub fn new(stream: TcpStream) -> Self {
         Self {
-            stream,
+            stream: BufWriter::new(stream),
             buffer: BytesMut::with_capacity(4096),
         }
     }
 
     fn parse_frame(&mut self) -> Result<Option<Frame>> {
-        Ok(Some(Frame::Null))
+        let mut buf = Cursor::new(&self.buffer[..]);
+
+        match Frame::check(&mut buf) {
+            Ok(_) => {
+                let len = buf.position() as usize;
+                buf.set_position(0);
+                let frame = Frame::parse(&mut buf)?;
+                self.buffer.advance(len);
+                Ok(Some(frame))
+            }
+            Err(Incomplete) => Ok(None),
+            Err(e) => Err(e.into()),
+        }
     }
 
     pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
@@ -35,12 +49,64 @@ impl Connection {
             }
         }
     }
+
+    pub async fn write_frame(&mut self, frame: &Frame) -> io::Result<()> {
+        match frame {
+            Frame::Simple(val) => {
+                self.stream.write_u8(b'+').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Error(val) => {
+                self.stream.write_u8(b'-').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Integer(val) => {
+                self.stream.write_u8(b':').await?;
+                self.write_decimal(*val).await?;
+            }
+            Frame::Null => {
+                self.stream.write_all(b"$-1\r\n").await?;
+            }
+            Frame::Bulk(val) => {
+                let len = val.len();
+                self.stream.write_u8(b'$').await?;
+                self.write_decimal(len as u64).await?;
+                self.stream.write_all(val).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Array(_val) => unimplemented!(),
+        }
+        self.stream.flush().await?;
+        Ok(())
+    }
+
+    async fn write_decimal(&mut self, val: u64) -> io::Result<()> {
+        use std::io::Write;
+
+        let mut buf = [0u8; 12];
+        let mut buf = Cursor::new(&mut buf[..]);
+        write!(&mut buf, "{}", val)?;
+        let pos = buf.position() as usize;
+        self.stream.write_all(&buf.get_ref()[..pos]).await?;
+        self.stream.write_all(b"\r\n").await?;
+        Ok(())
+    }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6739").await.unwrap();
 
-    let (socket, _) = listener.accept().await.unwrap();
-    let con = Connection::new(socket);
+    loop {
+        let (socket, _) = listener.accept().await?;
+        let mut con = Connection::new(socket);
+        println!("connection made");
+
+        let r = con.read_frame().await;
+        println!("read {:?}", r);
+        let resp = Frame::Integer(200);
+        con.write_frame(&resp).await?;
+    }
 }
