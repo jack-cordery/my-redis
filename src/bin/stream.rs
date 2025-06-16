e4ea23528ba88d5476420815641ce386e99ee64d
Jack Cordery <jack.redolf@gmail.com>
2025-06-16 16:32:08 +0100

    manual implementation of stream trait to create a stream that sends 3 messages at intervals of 10millis

--- src/bin/stream.rs
+++ src/bin/stream.rs
@@ -1,6 +1,62 @@
 use mini_redis::client;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use tokio::time::Instant;
 use tokio::time::{Duration, sleep};
-use tokio_stream::StreamExt;
+use tokio_stream::{Stream, StreamExt};
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<&'static str> {
+        if Instant::now() >= self.when {
+            Poll::Ready("done")
+        } else {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
+
+struct Interval {
+    rem: usize,
+    delay: Delay,
+}
+
+impl Interval {
+    pub fn new() -> Self {
+        Interval {
+            rem: 3,
+            delay: Delay {
+                when: Instant::now(),
+            },
+        }
+    }
+}
+
+impl Stream for Interval {
+    type Item = ();
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<()>> {
+        if self.rem == 0 {
+            return Poll::Ready(None);
+        }
+
+        match Pin::new(&mut self.delay).poll(cx) {
+            Poll::Ready(_) => {
+                self.rem -= 1;
+                let when = self.delay.when + Duration::from_millis(10);
+                self.delay = Delay { when };
+                Poll::Ready(Some(()))
+            }
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
 
 async fn publish() -> mini_redis::Result<()> {
     let mut connect = client::connect("127.0.0.1:6379").await?;
@@ -43,5 +99,13 @@ async fn main() -> mini_redis::Result<()> {
 
     subscribe().await?;
     println!("Done");
+
+    let mut interval = Interval::new();
+    while interval.next().await.is_some() {
+        println!(
+            "manual streamed delayed a few instances in time at {:?}",
+            Instant::now()
+        )
+    }
     Ok(())
 }
